ffe9641850087ac2c641a4bf8d4be74a3029a57b
Murilo Silva <murilo.alex.silva@gmail.com>
2020-08-28 10:56:28 -0300

    Subindo arquivos do app

---
+++ lib/contador.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Contador extends StatefulWidget {
+  @override
+  _ContadorState createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int quantidade = 0;
+
+  incrementar() {
+    //Metodo para incrementar
+    setState(() {
+      quantidade++;
+    });
+  }
+
+  decrementar() {
+    //metodo para decrementar
+    setState(() {
+      quantidade--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Contador"),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                child: Column(
+                  children: [
+                    Text(
+                      "Quantidade",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[500]),
+                    ),
+                    Text(
+                      "$quantidade",
+                      style: TextStyle(fontSize: 40, color: Colors.blue[100]),
+                    ),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.center,
+                      children: [
+                        FlatButton(
+                          onPressed: incrementar,
+                          child: Text("Incrementar",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                        FlatButton(
+                          onPressed: decrementar,
+                          child: Text("Decrementar",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:contador/contador.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Contador());
+}
